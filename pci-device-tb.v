cd3034a567f147aeb5d3aa261488246abac86abd
AbdoWahba <abdo.tw20154@gmail.com>
2018-12-23 00:15:13 +0200

    editing instance of device A and B and adding device C

--- pci-device-tb.v
+++ pci-device-tb.v
@@ -1,20 +1,25 @@
 module Device_tb();
     
-    reg clk, FREQA=1'b1,FREQB=1'b1;
-    wire [31:0] AD;
-    wire [3:0] C_BE;
-    wire REQA, REQB, FRAME, DEVSEL, IRDY, TRDY;
-    wire [4:0] GNT;
+    /**
+    *   PCI basic signals
+    */
+    reg clk;
+    wire [31:0] AD;    wire [3:0] C_BE;    wire [4:0] GNT;
+    wire REQA, REQB, REQC, FRAME, DEVSEL, IRDY, TRDY;
+    
+    /**
+    *   Make all signals HIGH by Default
+    */
+    pullup(FRAME);    pullup(IRDY);    pullup(TRDY);    pullup(DEVSEL);
 
     /**
-    *   Make all signals HIGH by default
+    *   Additional Signals
     */
-    pullup(FRAME);
-    pullup(IRDY);
-    pullup(TRDY);
-    pullup(DEVSEL);
+    reg FREQA=1'b1,FREQB=1'b1, FREQC=1'b1; // To force request Bus && determine number of required transactions
+    reg [31:0] TARGET_ADDRESS; // Select target to Communicate with
+    reg [3:0] OPERATION; // `WRITE_C_BE / `READ_C_BE
 
-    
+
     Device  devA(
         clk,
         REQA,
@@ -26,7 +31,10 @@ module Device_tb();
         DEVSEL,
         IRDY,
         TRDY ,
-        `DEVICE_A_ADDRESS
+        `DEVICE_A_ADDRESS,
+
+        TARGET_ADDRESS,
+        OPERATION
         );
 
     Device  devB(
@@ -40,44 +48,73 @@ module Device_tb();
         DEVSEL,
         IRDY,
         TRDY,
-        `DEVICE_B_ADDRESS
+        `DEVICE_B_ADDRESS,
+
+        TARGET_ADDRESS,
+        OPERATION
         );
 
+    Device  devc(
+        clk,
+        REQC,
+        FREQC,
+        GNT[2],
+        AD,
+        C_BE,
+        FRAME,
+        DEVSEL,
+        IRDY,
+        TRDY,
+        `DEVICE_C_ADDRESS,
+
+        TARGET_ADDRESS,
+        OPERATION
+        );
+
+
     wire [4:0] ARBREQ;
-    assign ARBREQ = {REQA,REQB,3'b111};
+    assign ARBREQ = {REQA,REQB,REQC,2'b11};
     arbitration arb(clk,1'b0,FRAME,ARBREQ ,IRDY,GNT);
 
-        initial begin
-            /**
-            *    $dumpfile("wave.vcd");
-            *    $dumpvars(0,Device_tb);
-            *       for gtkwave extinsion on linux 
-            */
-            $dumpfile("wave.vcd");
-            $dumpvars(0,Device_tb);
 
-            clk <= 0;
-        end
+    initial begin
+        /**
+        *    $dumpfile("wave.vcd");
+        *    $dumpvars(0,Device_tb);
+        *       for gtkwave extinsion on linux 
+        */
+        $dumpfile("wave.vcd");
+        $dumpvars(0,Device_tb);
 
-        always #1 clk = !clk;
+        clk <= 0;
+    end
 
-        initial begin
+    always #1 clk = !clk;
 
-            FREQA <= 1'b1;
-            #0.1 FREQA <= 1'b0;
-            #0.1 FREQA <= 1'b1;
-            #0.1 FREQA <= 1'b0;
-            #0.1 FREQA <= 1'b1;
-            #0.1 FREQA <= 1'b0;
-            #0.1 FREQA <= 1'b1;
+    initial begin
+        /**
+        *   force request Device A for 3 transactions
+        */
+        #0.1 FREQA <= 1'b0;
+        #0.1 FREQA <= 1'b1;
 
-        end
+        #0.1 FREQA <= 1'b0;
+        #0.1 FREQA <= 1'b1;
 
-        initial begin
-            #5 FREQB <= 1'b0;
-            #1 FREQB <= 1'b1;
-        end
+        #0.1 FREQA <= 1'b0;
+        #0.1 FREQA <= 1'b1;
+    end
 
-        initial #50 $finish;
+    initial begin
+        /**
+        *   force request Device B for 1 transaction after 5 unit time
+        */
+        #5 FREQB <= 1'b0;
+        #1 FREQB <= 1'b1;
+    end
+
+    initial #1000 $finish;
 
 endmodule
+
+
